5e890a33af2086bd9accead844b8ce38de19c3d8
nachotornati <nacho.tornati@gmail.com>
2020-09-09 22:43:00 -0300

    correcciones varias

--- sitio/app.js
+++ sitio/app.js
@@ -1,34 +1,34 @@
 const express = require('express');
 const app = express();
-app.use('/public', express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public"));
 
 app.get('/',(req,res)=>{
 
-    res.sendFile(__dirname + '/public/index.html');
+    res.sendFile(__dirname + '/index.html');
 
 });
 
 app.get('/carrito',(req,res)=>{
 
-    res.sendFile(__dirname + '/public/carrito.html');
+    res.sendFile(__dirname + '/carrito.html');
 
 });
 
 app.get('/detalle',(req,res)=>{
 
-    res.sendFile(__dirname + '/public/detalle.html');
+    res.sendFile(__dirname + '/detalle.html');
 
 });
 
 app.get('/login',(req,res)=>{
 
-    res.sendFile(__dirname + '/public/login.html');
+    res.sendFile(__dirname + '/login.html');
 
 });
 
 app.get('/registro',(req,res)=>{
 
-    res.sendFile(__dirname + '/public/registro.html');
+    res.sendFile(__dirname + '/registro.html');
 
 });
 
