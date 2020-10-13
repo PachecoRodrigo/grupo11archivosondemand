3d71ce9fe9d1a9eca657c9260950aa14fcd78808
nachotornati <nacho.tornati@gmail.com>
2020-10-13 16:43:16 -0300

    creacion del sitio con express-generator

--- FilesOnDemand/src/app.js
+++ FilesOnDemand/src/app.js
@@ -4,22 +4,22 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./src/routes/index');
-var usersRouter = require('./src/routes/users');
-var productsRouter = require('./src/routes/products');
-var cartRouter = require('./src/routes/cart');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var productsRouter = require('./routes/products');
+var cartRouter = require('./routes/cart');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'src','views'));
+app.set('views', path.join(__dirname,'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname,'..', 'public')));
 
 
 app.use('/', indexRouter);
