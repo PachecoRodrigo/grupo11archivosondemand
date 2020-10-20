0f31d0475ac9e6b273497d05bf15a9f40855da74
Christian Pacheco <christianpachecoayza@gmail.com>
2020-10-20 13:25:33 -0300

    Aded page all products and modify detail
    Se creo una base de datos para visualizar en la plantilla todos los productos y acceder a cada uno en detalle del producto

--- FilesOnDemand/src/controllers/productsController.js
+++ FilesOnDemand/src/controllers/productsController.js
@@ -1,10 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+
+// LEENDO LOS DATOS DE LOS PRODUCTOS JSON
+const productsFilePath = path.join(__dirname, '../data/productsDB.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+
 module.exports = {
     index: (req, res, next) => {
-        res.send('Vista de productos');
+        res.render('allProducts', { products });
       },
+ 
     detail: (req,res)=>{
-        res.render('detail');
-    },
+        let id = (req.params.idProduct);
+        let oneproduct = products.filter(function(element){
+            return element.idProduct == id;
+            });
+        res.render('detail',{ oneproduct });
+        },
     create: (req,res)=>{
         res.render('create-form');
     },
